14d6be56b7cfe9fd0f24bee1250c2f9bf9844283
Rushikesh <rushi96bhosale@gmail.com>
2022-06-01 23:00:37 +0530

    rushi3

--- src/Abstraction/Car.java
+++ src/Abstraction/Car.java
@@ -3,7 +3,7 @@ package Abstraction;
 public class Car extends Vehical {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("baramati");
 System.out.println("rushikesh bhosale");
 
 		
