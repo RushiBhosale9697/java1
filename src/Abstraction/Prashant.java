a229020e7a20ff9a975644f040d7fcac73cea4cb
prashant <prashant.pondkule9099@gmail.com>
2022-06-06 22:20:01 +0530

    xyz

--- src/Abstraction/Prashant.java
+++ src/Abstraction/Prashant.java
@@ -4,9 +4,13 @@ public class Prashant {
 	public static void abc() {
 		System.out.println("This is abc method");
 	}
+	public static void pqr() {
+		System.out.println("This is pqr method");
+	}
 
 	public static void main(String[] args) {
 		Prashant.abc();
+		Prashant.pqr();
 		
 	}
 
